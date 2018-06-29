b48ba1f86ada50bc3b703d647a1dfbdd136c13b7
Abdurahman Almonajed <abud_almonajed@hotmail.com>
2018-06-29 21:32:34 +0200

    object and array

--- examples/js_2/main.js
+++ examples/js_2/main.js
@@ -1,11 +1,32 @@
-function alertFunction () {
-  alert('juhu')
+function alertFunction() {
+    alert('juhu')
 }
 
-function changeText () {
-  document.getElementById('text').innerHTML = variable + ' ' + number + ' ' + numberStr
+function changeText() {
+    document.getElementById('text').innerHTML = variable + ' ' + number + ' ' + numberStr
+}
+
+function changeText2() {
+    document.getElementById('text').innerHTML = array + ' ' + array[2]
+}
+
+function changeText3() {
+    document.getElementById('text').innerHTML = arrayStr
+}
+
+function changeText4() {
+    document.getElementById('text').innerHTML = color.name + " " + color.like + " " + color.favourite
 }
 
 var number = 5
 var variable = 'juhu abu'
 var numberStr = '10'
+
+var array = [0, 1, 2, 3, 4]
+var arrayStr = ['A', 'B', 'U']
+
+var color = {
+    name: "Red",
+    favourite: "no",
+    like: "yes"
+}
