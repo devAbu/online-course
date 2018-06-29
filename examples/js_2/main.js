48a0e1ec5282b3d2febc0e0b9e2c4125e4a4ddff
Abdurahman Almonajed <abud_almonajed@hotmail.com>
2018-06-29 21:15:59 +0200

    simple var

--- examples/js_2/main.js
+++ examples/js_2/main.js
@@ -1,3 +1,11 @@
-function alertFunction(){
-    alert("juhu");
+function alertFunction () {
+  alert('juhu')
 }
+
+function changeText () {
+  document.getElementById('text').innerHTML = variable + ' ' + number + ' ' + numberStr
+}
+
+var number = 5
+var variable = 'juhu abu'
+var numberStr = '10'
