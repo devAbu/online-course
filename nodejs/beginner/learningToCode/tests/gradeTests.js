23f6c6855b7eb53c61051a579df071af22808876
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2020-07-20 18:50:11 +0200

    learning tests

---
+++ nodejs/beginner/learningToCode/tests/gradeTests.js
@@ -0,0 +1,26 @@
+var book = require('../lib/grades').book
+
+exports["setUp"] = function (callback) {
+    book.reset()
+    callback()
+}
+
+exports["Can average grades"] = function (test) {
+    book.addGrade(100)
+    book.addGrade(50)
+
+    var average = book.getAverage()
+
+    test.equal(average, 75)
+    test.done()
+}
+
+exports["Can add new grade"] = function (test) {
+
+    book.addGrade(90)
+    var count = book.getCountOfGrade()
+
+    test.equal(count, 1)
+
+    test.done()
+}
