1184a166d15410e0e7bbe609f862cadb1cdb6544
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2020-07-22 18:08:39 +0200

    EJS

--- nodejs/intermediate/buildnigApp-Nodejs-Express/library/app.js
+++ nodejs/intermediate/buildnigApp-Nodejs-Express/library/app.js
@@ -18,7 +18,8 @@ app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist/')));
 
 app.set('views', './src/views');
-app.set('view engine', 'pug');
+/* app.set('view engine', 'pug'); */
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   /* res.sendFile(__dirname + '/views/index.html') */
