5f52e9225640ca231c05fb1623e490ce8a35b3e8
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2020-07-22 17:59:40 +0200

    PUG

--- nodejs/intermediate/buildnigApp-Nodejs-Express/library/app.js
+++ nodejs/intermediate/buildnigApp-Nodejs-Express/library/app.js
@@ -17,10 +17,17 @@ app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist/')));
 
+app.set('views', './src/views');
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
   /* res.sendFile(__dirname + '/views/index.html') */
-  res.sendFile(path.join(__dirname, 'views/index.html'));
+  /* res.sendFile(path.join(__dirname, 'views/index.html')); */
   /* res.sendFile(path.join(__dirname, 'views', 'index.html')) */
+  res.render('index', {
+    title: 'My Library',
+    list: ['a', 'b', 'c'],
+  });
 });
 
 app.listen(port, () => {
