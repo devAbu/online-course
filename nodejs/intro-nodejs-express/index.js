d3e58d802631c611a30905fb2b0b0201d8c721b3
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2020-08-05 12:27:38 +0200

    done2

--- nodejs/intro-nodejs-express/index.js
+++ nodejs/intro-nodejs-express/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+var bodyParser = require('body-parser')
 
 const app = express();
 
@@ -7,6 +8,29 @@ app.set('views', path.join(__dirname, '/views')); // views directory
 app.set('view engine', 'mustache'); //view engine
 app.engine('mustache', require('hogan-middleware').__express) // to enable mustache
 app.use(express.static(path.join(__dirname, 'public')));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// middleware
+
+/* app.use((req, res, next) => {
+  req.timestamp = new Date()
+  next()
+}) */
+
+const timestamp = (req, res, next) => {
+  req.timestamp = new Date()
+  next()
+}
+
+app.use(timestamp)
+
 
 const indexRouter = require('./routes/index')
 
--- nodejs/intro-nodejs-express/routes/index.js
+++ nodejs/intro-nodejs-express/routes/index.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
+const profiles = {
+  abu: {
+    name: 'abu',
+    company: 'self',
+    languages: ["nodejs", "js", "php"],
+    image: '../images/image1.png'
+  },
+  combe: {
+    name: 'combe',
+    company: 'aboc',
+    languages: ["html", "bootstrap", "css"]
+  },
+  kum: {
+    name: 'kum',
+    company: 'knjiga',
+    languages: ["mysql", "mongodb", "sql"]
+  },
+}
+
 router.get('/', (req, res, next) => {
   res.send('Hello');
 })
 
 router.get('/json', (req, res, next) => {
   let data = {
-    greeting: 'Hello'
+    greeting: 'Hello',
+    timestamp: req.timestamp.toString()
   }
   res.json(data);
   /* res.json({
@@ -16,7 +36,67 @@ router.get('/json', (req, res, next) => {
 })
 
 router.get('/home', (req, res, next) => {
-  res.render('home', null);
+  res.render('home', {
+    text: 'Dynamic data'
+  });
+})
+
+router.get('/query', (req, res) => {
+  const name = req.query.name
+  const age = req.query.age
+
+  const data = {
+    name, // == name: name
+    age, // age: age
+  }
+
+  res.render('profile', data) // req.query
+})
+
+router.get('/:param', (req, res) => {
+  const param = req.params.param
+
+  res.json({
+    data: param
+  })
+})
+
+/* router.get('/:profile/:username', (req, res) => {
+  const profile = req.params.profile
+  const username = req.params.username
+
+  res.json({
+    profile: profile,
+    username: username
+  })
+}) */
+
+router.get('/:profile/:username', (req, res) => {
+  const profile = req.params.profile
+  const username = req.params.username
+  const currentProfile = profiles[profile]
+
+  if (currentProfile == null) {
+    res.json({
+      confirmation: 'failed',
+      message: 'Profile ' + profile + ' not found!'
+    })
+  } else {
+    /* res.json({
+      confirmation: success,
+      profile: currentProfile
+    }) */
+    res.render('profile', currentProfile)
+  }
+})
+
+router.post('/post', (req, res) => {
+  const body = req.body // POST form
+
+  res.json({
+    confirmation: 'success',
+    data: body
+  })
 })
 
 module.exports = router;
