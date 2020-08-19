5c46c2af30f3db2966905f4de81b4aed0b7937b0
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2020-08-19 18:22:32 +0200

    Class finished

--- typescript/classes-interfaces/dist/app.js
+++ typescript/classes-interfaces/dist/app.js
@@ -4,9 +4,10 @@ class Department {
         this.ID = ID;
         this.name = name;
         this.employees = [];
+        console.log(Department.year);
     }
-    describe() {
-        console.log(`Department ${this.ID}:  ${this.name}`);
+    static createEmployee(name) {
+        return { name };
     }
     addEmployee(employee) {
         this.employees.push(employee);
@@ -15,19 +16,41 @@ class Department {
         console.log(this.employees.length, this.employees);
     }
 }
+Department.year = 2020;
 class ITDepartment extends Department {
     constructor(id, admins) {
         super(id, 'IT');
         this.admins = admins;
+    }
+    describe() {
+        console.log('IT department');
     }
 }
 class AccountingDepartment extends Department {
     constructor(id, reports) {
         super(id, 'Accounting');
         this.reports = reports;
+        this.lastReport = reports[0];
+    }
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error('No report found');
+    }
+    set mostRecentReport(value) {
+        this.addReport(value);
+    }
+    static getInstance() {
+        if (AccountingDepartment.instance) {
+            return this.instance;
+        }
+        this.instance = new AccountingDepartment('d2', []);
+        return this.instance;
     }
     addReport(text) {
         this.reports.push(text);
+        this.lastReport = text;
     }
     addEmployee(name) {
         if (name === 'Abu') {
@@ -38,15 +61,24 @@ class AccountingDepartment extends Department {
     printReports() {
         console.log(this.reports);
     }
+    describe() {
+        console.log('Accounting department');
+    }
 }
 const ITAccounting = new ITDepartment('555', ['Abu']);
 console.log(ITAccounting);
 ITAccounting.addEmployee('Abu');
-console.log(ITAccounting);
-const accounting = new AccountingDepartment('999', ['juhu', 'huju']);
+ITAccounting.printEmployeeInfo();
+ITAccounting.describe();
+const accounting = AccountingDepartment.getInstance();
+console.log(accounting);
 accounting.addEmployee('Combe');
 accounting.addEmployee('Abu');
+accounting.mostRecentReport = 'Report';
 accounting.addReport('something went wrong');
+console.log(accounting.mostRecentReport);
 accounting.printReports();
-console.log(accounting);
+accounting.describe();
+const employee1 = Department.createEmployee('Abu static');
+console.log(employee1, Department.year);
 //# sourceMappingURL=app.js.map
--- typescript/classes-interfaces/src/app.ts
+++ typescript/classes-interfaces/src/app.ts
@@ -1,18 +1,30 @@
-class Department {
+/*Abstract class - Can not be initiate */
+/* Singleton class - can have only one instance - private constructor + static method */
+abstract class Department {
   /* private readonly ID: string;
   name: string;  // default is PUBLIC*/
+  static year = 2020;
   protected employees: string[] = [];
   /* private - only accessible inside the class */
   /* protected - accessible from that class and all subclasses (inheritance) */
 
-  constructor(private readonly ID: string, public name: string) { //readonly - can not change after initialization
+  /* Constructor can not be static */
+  constructor(protected readonly ID: string, public name: string) { //readonly - can not change after initialization
     /* this.ID = ID;
      this.name = n; */
+    /* console.log(this.year) ERROR , can not access static property or method inside the class with this */
+    console.log(Department.year);
   }
 
-  describe(this: Department) {
-    console.log(`Department ${this.ID}:  ${this.name}`);
+  static createEmployee(name: string) {
+    return { name };
   }
+  /* describe(this: Department) {
+    console.log(`Department ${this.ID}:  ${this.name}`);
+  } */
+
+  /* ABSTRACT CLASS - need to be implemented in all subclasses*/
+  abstract describe(this: Department): void;
 
   addEmployee(employee: string) {
     /* this.ID = '321'; ERROR - can not change it because it is readonly*/
@@ -30,15 +42,45 @@ class ITDepartment extends Department {
     super(id, 'IT');/* CALL CONSTRUCT FROM INHERITANCE CLASS */
     this.admins = admins;
   }
+
+  describe() {
+    console.log('IT department');
+  }
 }
 
 class AccountingDepartment extends Department {
-  constructor(id: string, private reports: string[]) {
+  private lastReport: string;
+  private static instance: AccountingDepartment;
+
+  /* GETTERS - WITH GET lastReport became publicly accessible */
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error('No report found');
+  }
+
+  /* SETTER -  */
+  set mostRecentReport(value: string) {
+    this.addReport(value);
+  }
+
+  static getInstance() {
+    if (AccountingDepartment.instance) {
+      return this.instance;
+    }
+    this.instance = new AccountingDepartment('d2', []);
+    return this.instance;
+  }
+
+  private constructor(id: string, private reports: string[]) {
     super(id, 'Accounting');
+    this.lastReport = reports[0];
   }
 
   addReport(text: string) {
     this.reports.push(text);
+    this.lastReport = text;
   }
 
   /* OVERRIDING - Override the method from the main/inheritance class. Method name, parameters and return type must be the same */
@@ -52,6 +94,10 @@ class AccountingDepartment extends Department {
 
   printReports() {
     console.log(this.reports);
+  }
+
+  describe() {
+    console.log('Accounting department');
   }
 }
 
@@ -79,11 +125,23 @@ const ITAccounting = new ITDepartment('555', ['Abu']);
 console.log(ITAccounting);
 
 ITAccounting.addEmployee('Abu');
-console.log(ITAccounting);
+ITAccounting.printEmployeeInfo();
+ITAccounting.describe()
 
-const accounting = new AccountingDepartment('999', ['juhu', 'huju']);
+/* const accounting = new AccountingDepartment('999', []); */
+const accounting = AccountingDepartment.getInstance();
+console.log(accounting)
+/* console.log(accounting.mostRecentReport); */
+
 accounting.addEmployee('Combe');
 accounting.addEmployee('Abu'); // will not be added
+accounting.mostRecentReport = 'Report' //SET
 accounting.addReport('something went wrong');
+console.log(accounting.mostRecentReport); // GET
 accounting.printReports();
-console.log(accounting);
+accounting.describe()
+/* new Math() - no need
+Math.pow() - static example */
+/* STATIC - access without initiate */
+const employee1 = Department.createEmployee('Abu static')
+console.log(employee1, Department.year)
